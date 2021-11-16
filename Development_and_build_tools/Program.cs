f4af06f308ad9ad51802a62d98ee5f22ca011b4a
Pavel Marchenko <Pavel Marchenko@DESKTOP-ONM0F4H>
2021-11-16 20:37:19 +0300

    Добавьте файлы проекта.

---
+++ Development_and_build_tools/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Development_and_build_tools
+{
+    static void Main(string[] args)
+    {
+        SortedSet<char> set_args = new SortedSet<char>();
+
+        for(int i = 0; i < args.Length; i++)
+        {
+            foreach (char arg in args[i])
+            {
+                set_args.Add(arg);
+            }
+        }
+
+        foreach (char arg in set_args) { Console.WriteLine(arg); }
+    }
+}
